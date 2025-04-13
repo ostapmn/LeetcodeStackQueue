aef17fdf5022c45db4ff1cf29d25b9a61a8d8747
Ostap Mnykh <mnykh.pn@ucu.edu.ua>
2025-04-13 15:20:25 +0300

    No dict, timeout error 27/38 passed

--- Maximum Frequency Stack/main.py
+++ Maximum Frequency Stack/main.py
@@ -16,14 +16,14 @@ class FreqStack:
 
     def pop(self) -> int:
         max_freq = (0, None)
-        # for _ in range(len(self.stack)):
-        #     x = self.stack.popleft()
-        #     self.stack.append(x)
-        #     if self.stack.count(x) >= max_freq[0]:
-        #         max_freq = (self.stack.count(x), x)
-        for el in self.stack:
-            if self.stack.count(el) >= max_freq[0]:
-                max_freq = (self.stack.count(el), el)
+        for _ in range(len(self.stack)):
+            x = self.stack.popleft()
+            self.stack.append(x)
+            if self.stack.count(x) >= max_freq[0]:
+                max_freq = (self.stack.count(x), x)
+        # for el in self.stack:
+        #     if self.stack.count(el) >= max_freq[0]:
+        #         max_freq = (self.stack.count(el), el)
         res = max_freq[1]
         for _ in range(len(self.stack)):
             x = self.stack.pop()
@@ -50,9 +50,3 @@ print(freqStack.pop())
 print(freqStack.stack)
 print(freqStack.pop())
 print(freqStack.stack)
-# for el in freqStack.stack:
-#     print(el, end = ' ')
-#ject will be instantiated and called as such:
-# obj = FreqStack()
-# obj.push(val)
-# param_2 = obj.pop()
